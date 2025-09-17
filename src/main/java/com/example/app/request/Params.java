78304349c999843101e9334704ad453f63b80178
appwavesolutions <159064548+appwavesolutions@users.noreply.github.com>
2025-09-17 18:58:13 +0100

    chore(build): update PostgreSQL version in docker-compose and adjust port; add fields to Params class

--- src/main/java/com/example/app/request/Params.java
+++ src/main/java/com/example/app/request/Params.java
@@ -2,4 +2,22 @@ package com.example.app.request;
 
 public class Params {
 	// Define arbitrary fields later; stored as JSONB in DB
+	private String name;
+	private String path;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
 }
